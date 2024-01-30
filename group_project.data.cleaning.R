c41cb4910295f05b18a5a450f16ce6056355f12d
AlessandraGolini <146853793+AlessandraGolini@users.noreply.github.com>
2024-01-30 16:13:30 +0100

    Update group_project.data.cleaning.R

--- group_project.data.cleaning.R
+++ group_project.data.cleaning.R
@@ -6,16 +6,13 @@ setwd()
 # ale "C:/Users/aless/OneDrive - Alma Mater Studiorum Università di Bologna/Global Change Ecology and SDGs/Statistical Analysis and Modelling/project"
 # sere
 
-# import the dataset using the read.csv() function
-# the separator is set to be the semicolon (sep = ";")
-# the first row of the dataset describes the name of the variables and it is mantained setting the parameter header = T
-read.csv("solar_weather.csv",
-         header=T, sep=";",
-         stringsAsFactors = T)
+         # To import the dataset we use the read.csv() function
+         # the separator is set to be the semicolon (sep = ";")
+         # the first row of the dataset describes the name of the variables and it is mantained setting the parameter header = T
+read.csv("solar_weather.csv", header=T, sep=";", stringsAsFactors = T)
 
-# INSTALL AND OPNE PACKAGES 
-# capiamo se ci serve o no (????)
 
 ## ACTUAL DATA CLEANING
+         # To rename the variables 
 
 
